0b7ae9dce73f9350fce287f416479040392f8867
Stephen Archer <s.archer@f5.com>
2024-01-08 20:58:09 +0000

    added as3 day 2

--- terraform-aws/as3-day2/app_configs/production/london/main.tf
+++ terraform-aws/as3-day2/app_configs/production/london/main.tf
@@ -2,7 +2,7 @@ data "terraform_remote_state" "aws_demo" {
   backend = "local"
 
   config = {
-    path = "${path.module}/../terraform.tfstate"
+    path = "${path.module}/../../../../terraform.tfstate"
   }
 }
 
@@ -12,14 +12,24 @@ provider "bigip" {
   password = data.terraform_remote_state.aws_demo.outputs.f5_password
 }
 
-
 resource "bigip_as3" "nginx" {
-
-  as3_json = templatefile("./as3_vs2.tpl", {
-    vs2_ip = jsonencode([data.terraform_remote_state.aws_demo.outputs.f5_vs2[0]])
+  as3_json     = templatefile("./as3_vs2.tpl", {
+    vs2_ip     = jsonencode([data.terraform_remote_state.aws_demo.outputs.f5_vs2[0]])
+    cert       = var.cert
+    key        = var.key
+    ciphertext = base64encode(var.ciphertext)
+    protected  = base64encode(var.protected)
   })
-
 }
 
+resource "local_file" "nginx" {
+  content       = templatefile("./as3_vs2.tpl", {
+    vs2_ip     = jsonencode([data.terraform_remote_state.aws_demo.outputs.f5_vs2[0]])
+    cert       = var.cert
+    key        = var.key
+    ciphertext = base64encode(var.ciphertext)
+    protected  = base64encode(var.protected)
 
-
+  })
+  filename = "./rendered-as3.json"
+}
---
+++ terraform-aws/as3-day2/app_configs/production/london/vars.tf
@@ -0,0 +1,15 @@
+variable "cert" {
+  type = string
+}
+
+variable "key" {
+  type = string
+}
+
+variable "ciphertext" {
+  type = string
+}
+
+variable "protected" {
+  type = string
+}
